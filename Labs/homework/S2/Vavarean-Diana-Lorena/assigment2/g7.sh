e44931f7b78473d9d853f8805795b866c55675bd
Diana Lorena Vararean <vararean.diana@gmail.com>
2025-03-24 19:46:09 +0200

    lab5: homework 7

---
+++ Labs/homework/S2/Vavarean-Diana-Lorena/assigment2/g7.sh
@@ -0,0 +1,3 @@
+#Pb g7 - Write a shell script which takes as parameters several host names(ex: www.cs.ubbcluj.ro www.google.ro). The script will display the host names (from those given as parameters) that are alive. Use the ping command to verify that a given host is alive. 
+
+#!/bin/bash

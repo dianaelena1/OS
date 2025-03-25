7b1b7b90a7c3a4a49525b102f89c93f4b8c37014
silag <silaghiraul13@gmail.com>
2025-03-25 18:51:57 +0200

    Silaghi Raul - assigment1

---
+++ Labs/homework/S2/Silaghi-Raul-Emanuel/assigments/assigment1/assigment1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Write a shell script that reads words from keyboard (the reading stops when the user has entered the word "stop"). The script will display the list of words entered from the keyboard.
+words=()
+echo "Enter the words. Type 'stop' to finish."
+
+
+while true; do
+  read -p "Enter word: " word
+
+  if [ "$word" = "stop" ]; then
+    break
+  fi
+
+  words+=("$word")
+done
+
+echo "You entered the following words: "
+for word in "${words[@]}"; do
+  echo "$word"
+done

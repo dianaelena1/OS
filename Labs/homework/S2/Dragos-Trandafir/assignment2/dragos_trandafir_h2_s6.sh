e3e76d9003e6bf2bde0b26bf01cffc7474a578df
DragosTrandafiri <dragostrandafir333@gmail.com>
2025-03-31 18:51:54 +0300

    Edited homework

--- Labs/homework/S2/Dragos-Trandafir/assignment2/dragos_trandafir_h2_s6.sh
+++ Labs/homework/S2/Dragos-Trandafir/assignment2/dragos_trandafir_h2_s6.sh
@@ -5,15 +5,15 @@
 #echo "Read lowercase letter"
 #read lc_letter
 
-if [ $# -le 1 ]; then
+if [ $# -le 2 ]; then
   echo "Number of arguments is invalid, input at least 2 parameters"
 else
 
-lc_letter=$1   # first parameter (the letter)
+lc_letter=$1   
 echo "$lc_letter"
 
-for ((i=2; i<=$#; i++)); do # do this for all the files
-  eval file=\$$i # use escape character \ and eval to actually compute the expression
-  sed -i "s/#/${lc_letter}/g" $file   # replace globally # with the lower case letter
+for ((i=2; i<=$#; i++)); do 
+  eval file=\$$i 
+  sed -i "s/#/${lc_letter}/g" $file   
 done
 fi

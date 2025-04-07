93c373999105bf157d08b5e94f864eb64e080b0d
GabrielRMNC <romanicagabrielmail@gmail.com>
2025-04-07 13:27:34 +0300

    added assignment2

---
+++ Labs/homework/S2/Romanica-Gabriel/assignment2/assignment2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 filename word1 [word2 ... wordN]"
+    exit 1
+fi
+
+file="$1"
+
+# Check if file exists
+if [ ! -f "$file" ]; then
+    echo "Error: File '$file' not found."
+    exit 2
+fi
+
+shift
+
+temp_file=$(mktemp)
+
+cp "$file" "$temp_file"
+
+for word in "$@"; do
+    sed -i "s/\b$word\b//g" "$temp_file"
+done
+
+mv "$temp_file" "$file"
+
+echo "Words removed successfully."

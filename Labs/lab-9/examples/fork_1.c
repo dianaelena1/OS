2d2c8c0911440f6533bca5670099ff459e679704
Diana Horincar <Diana.Horincar@msg.group>
2025-05-02 14:55:38 +0200

    lab-9:

--- Labs/lab-9/examples/fork_1.c
+++ Labs/lab-9/examples/fork_1.c
@@ -1,28 +1,32 @@
-/* fork_1.c
-Create a child process using fork()
-*/
+//
+// fork_1.c
+//
+// Using fork() to create a child process
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+ 
 int main(int argc, char* argv[])
 {
-int a = 5;
-int pid = fork(); // try to create a child process
-if (pid == -1) // fork() has failed
-{
-perror("fork() error\n");
-exit(EXIT_FAILURE); //exit(1);
-}
-if (pid == 0) // in the child process
-{
-printf("[In CHILD] My PID is %d. My parent PID is %d.\n", getpid(), getppid());
-exit(EXIT_SUCCESS); //exit(0);
-}
-else // in the parent process
-{
-printf("[In PARENT] My PID is %d. My child PID is %d.\n", getpid(), pid);
-int status;
-wait(&status); // wait(0);
-}
-return 0;
+    int pid = fork();
+    if (pid == -1)					// fork() failed
+    {
+        perror("fork() error");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0)					// in child process
+    {
+        printf("[In CHILD]\tMy PID is %d\tParent PID is %d\n", getpid(), getppid());
+		exit(EXIT_SUCCESS);
+    }
+    else							// in parent process
+    {
+        printf("[In PARENT]\tMy PID is %d\tChild PID is %d\n", getpid(), pid);
+    }
+
+    return 0;
 }

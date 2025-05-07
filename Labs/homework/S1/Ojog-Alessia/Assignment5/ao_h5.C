3e012848338a19eaebb5ec0416dc0084630cfa98
syalle <ojogalessiaelena@gmail.com>
2025-05-07 16:45:03 +0300

    Homework 6

--- Labs/homework/S1/Ojog-Alessia/Assignment5/ao_h5.C
+++ Labs/homework/S1/Ojog-Alessia/Assignment5/ao_h5.C
@@ -9,29 +9,25 @@ int main(int argc, char *argv[]) {
     size_t read;
     int line_number = 1;
 
-    // Check for proper usage
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
         return 1;
     }
 
-    // Open the file
+  
     file = fopen(argv[1], "r");
     if (file == NULL) {
         perror("Error opening file");
         return 1;
     }
 
-    // Read each line and count characters
     while ((read = getline(&line, &len, file)) != -1) {
-        // Remove newline character from count if present
         if (line[read - 1] == '\n') {
             read--;
         }
         printf("Line %d: %zd characters\n", line_number++, read);
     }
 
-    // Clean up
     free(line);
     fclose(file);
 

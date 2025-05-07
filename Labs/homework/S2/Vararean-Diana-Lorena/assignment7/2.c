66253ba916cf1575f56ee9b024c4adf34e6dece6
Diana Lorena Vararean <vararean.diana@gmail.com>
2025-05-07 20:00:14 +0300

    Assignment 7 - Pb 2

--- Labs/homework/S2/Vararean-Diana-Lorena/assignment7/2.c
+++ Labs/homework/S2/Vararean-Diana-Lorena/assignment7/2.c
@@ -10,122 +10,98 @@
 
 
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#include <unistd.h>
 #include <sys/wait.h>
 
-#define BUF_SIZE 100
-
-void error(const char *msg) {
-    perror(msg);
-    exit(1);
-}
-
-void replace_and_sum(char *buf, char *out_buf) {
-    int sum = 0;
-    for (int i = 0; buf[i]; i++) {
+void replace_and_sum(char *buf, int *sum) {
+    *sum = 0;
+    for (int i = 0; i < 10; i++) {
         if (isdigit(buf[i])) {
-            sum += buf[i] - '0';
+            *sum += buf[i] - '0';
         } else if (isalpha(buf[i])) {
             buf[i] = 'Z';
         }
     }
-    snprintf(out_buf, BUF_SIZE, "%s\nSum: %d", buf, sum);
-}
-
-void sort_string(char *str) {
-    int len = strlen(str);
-    for (int i = 0; i < len - 1; i++)
-        for (int j = i + 1; j < len; j++)
-            if (str[i] > str[j]) {
-                char temp = str[i];
-                str[i] = str[j];
-                str[j] = temp;
-            }
 }
 
 int main() {
-    int p1_to_2[2], p1_to_4[2], p1_to_5[2], p2_to_3[2], p3_to_4[2];
-    char buffer[BUF_SIZE];
-
-    pipe(p1_to_2);
-    pipe(p1_to_4);
-    pipe(p1_to_5);
-    pipe(p2_to_3);
-    pipe(p3_to_4);
-
-    // Process 1
-    pid_t pid1 = fork();
-    if (pid1 == 0) {
-        close(p1_to_2[0]); close(p1_to_4[0]); close(p1_to_5[0]);
-        printf("Process 1: Enter 10 characters:\n");
-        fgets(buffer, 11, stdin); // read 10 chars
-
-        write(p1_to_2[1], buffer, strlen(buffer) + 1);
-        write(p1_to_4[1], buffer, strlen(buffer) + 1);
-        write(p1_to_5[1], buffer, strlen(buffer) + 1);
-
-        exit(0);
+    int p1_to_2[2], p1_to_3[2], p1_to_4[2], p2_to_5[2], p3_to_5[2];
+    if (pipe(p1_to_2) < 0 || pipe(p1_to_3) < 0 || pipe(p1_to_4) < 0 || pipe(p2_to_5) < 0 || pipe(p3_to_5) < 0) {
+        perror("pipe error");
+        exit(1);
     }
 
-    // Process 2
-    pid_t pid2 = fork();
-    if (pid2 == 0) {
-        close(p1_to_2[1]); close(p2_to_3[0]);
+    pid_t pids[5];
+    for (int i = 0; i < 5; i++) {
+        pids[i] = fork();
+        if (pids[i] < 0) {
+            perror("fork error");
+            exit(1);
+        }
+        if (pids[i] == 0) {
+            if (i == 0) { // Process 1
+                close(p1_to_2[0]); close(p1_to_3[0]); close(p1_to_4[0]);
+                char buf[11];
+                read(STDIN_FILENO, buf, 10);
+                buf[10] = '\0';
+                write(p1_to_2[1], buf, 10);
+                write(p1_to_3[1], buf, 10);
+                write(p1_to_4[1], buf, 10);
+            } else if (i == 1) { // Process 2
+                close(p1_to_2[1]); close(p2_to_5[0]);
+                char buf[11];
+                int sum;
+                read(p1_to_2[0], buf, 10);
+                buf[10] = '\0';
+                replace_and_sum(buf, &sum);
+                write(p2_to_5[1], buf, 10);
+                write(p2_to_5[1], &sum, sizeof(int));
+            } else if (i == 2) { // Process 3
+                close(p1_to_3[1]); close(p3_to_5[0]);
+                char buf[11];
+                int sum;
+                read(p1_to_3[0], buf, 10);
+                buf[10] = '\0';
+                replace_and_sum(buf, &sum);
+                write(p3_to_5[1], buf, 10);
+                write(p3_to_5[1], &sum, sizeof(int));
+            } else if (i == 3) { // Process 4
+                close(p1_to_4[1]);
+                char buf[11];
+                read(p1_to_4[0], buf, 10);
+                buf[10] = '\0';
 
-        read(p1_to_2[0], buffer, BUF_SIZE);
-        char modified[BUF_SIZE];
-        replace_and_sum(buffer, modified);
-        write(p2_to_3[1], modified, strlen(modified) + 1);
-
-        exit(0);
+                for (int j = 0; j < 9; j++)
+                    for (int k = 0; k < 9 - j; k++)
+                        if (buf[k] > buf[k + 1]) {
+                            char t = buf[k];
+                            buf[k] = buf[k + 1];
+                            buf[k + 1] = t;
+                        }
+                printf("Process 4: ");
+                for(int j=0; j<=9; j++)
+                    printf("%d", buff[i]);
+            } else if (i == 4) { //  Process 5
+                close(p2_to_5[1]); close(p3_to_5[1]);
+                char buf_from_2[11], buf_from_3[11];
+                int sum_from_2, sum_from_3;
+                read(p2_to_5[0], buf_from_2, 10);
+                buf_from_2[10] = '\0';
+                read(p2_to_5[0], &sum_from_2, sizeof(int));
+                read(p3_to_5[0], buf_from_3, 10);
+                buf_from_3[10] = '\0';
+                read(p3_to_5[0], &sum_from_3, sizeof(int));
+                printf("Received from Process 2: %s, Sum: %d\n", buf_from_2, sum_from_2);
+                printf("Received from Process 3: %s, Sum: %d\n", buf_from_3, sum_from_3);
+            }
+            exit(0);
+        }
     }
 
-    // Process 3
-    pid_t pid3 = fork();
-    if (pid3 == 0) {
-        close(p2_to_3[1]); close(p3_to_4[0]);
-
-        read(p2_to_3[0], buffer, BUF_SIZE);
-        write(p3_to_4[1], buffer, strlen(buffer) + 1);
-
-        exit(0);
-    }
-
-    // Process 4
-    pid_t pid4 = fork();
-    if (pid4 == 0) {
-        close(p1_to_4[1]); close(p3_to_4[1]);
-
-        char from_p1[BUF_SIZE], from_p3[BUF_SIZE];
-        read(p1_to_4[0], from_p1, BUF_SIZE);
-        read(p3_to_4[0], from_p3, BUF_SIZE);
-
-        strcat(from_p1, from_p3);
-        sort_string(from_p1);
-
-        printf("Process 4 (sorted result): %s\n", from_p1);
-        exit(0);
-    }
-
-    // Process 5
-    pid_t pid5 = fork();
-    if (pid5 == 0) {
-        close(p1_to_5[1]);
-        read(p1_to_5[0], buffer, BUF_SIZE);
-        printf("Process 5 received: %s\n", buffer);
-        exit(0);
-    }
-
-    close(p1_to_2[0]); close(p1_to_2[1]);
-    close(p1_to_4[0]); close(p1_to_4[1]);
-    close(p1_to_5[0]); close(p1_to_5[1]);
-    close(p2_to_3[0]); close(p2_to_3[1]);
-    close(p3_to_4[0]); close(p3_to_4[1]);
-
-    for (int i = 0; i < 5; i++) wait(NULL);
-
+    for (int i = 0; i < 5; i++)
+        wait(NULL);
     return 0;
 }

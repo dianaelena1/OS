969a50ad7ac3aafbc96ed79be140eec52783171d
Diana Lorena Vararean <vararean.diana@gmail.com>
2025-05-07 18:32:59 +0300

    Assignment 6 - Pb 8

---
+++ Labs/homework/S2/Vararean-Diana-Lorena/assignment6/8.c
@@ -0,0 +1,58 @@
+//Pb 8 - Write a C Program that receives as command line argument
+// the name of a file and deletes the even numbers from this file.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int is_even(int n) {
+    return n % 2 == 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    pid_t pid = fork();
+
+    if (pid < 0) {
+        perror("fork failed");
+        return 1;
+    } 
+    else if (pid == 0) {
+        FILE *in = fopen(argv[1], "r");
+        if (!in) {
+            perror("Failed to open file");
+            exit(1);
+        }
+
+        FILE *temp = tmpfile();
+        int num;
+
+        while (fscanf(in, "%d", &num) == 1) {
+            if (num % 2 != 0) {
+                fprintf(temp, "%d ", num);
+            }
+        }
+
+        rewind(temp);
+        freopen(argv[1], "w", in);
+
+        while (fscanf(temp, "%d", &num) == 1) {
+            fprintf(in, "%d ", num);
+        }
+
+        fclose(in);
+        fclose(temp);
+        exit(0);
+    } 
+    else {
+        wait(NULL);
+        printf("Even numbers removed by child process.\n");
+    }
+
+    return 0;
+}
---
+++ Labs/homework/S2/Vararean-Diana-Lorena/assignment7/2.c
@@ -0,0 +1,11 @@
+//Pb 2 - Implement the following structure of processes, in which the vertices
+// indicate the communication between processes using pipes:
+
+//![](Aspose.Words.8d0edccb-2f34-4293-95e6-66be4d8c1cbf.002.png)
+
+//Process 1: read from stdin 10 bytes and write to each process 1,2 and 4
+//Processes 2, 3: add digits and replace letters with ‘Z’
+//Process 4: sort the result and print it
+//Process 5: print the information received by pipe
+
+

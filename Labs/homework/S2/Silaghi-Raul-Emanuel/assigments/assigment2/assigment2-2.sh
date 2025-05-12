13d9bab5b2d6971baf8bf69fa141d58eb6adc2ca
silag <silaghiraul13@gmail.com>
2025-05-12 18:03:26 +0300

    Assigment 5  - Silaghi Raul

--- Labs/homework/S2/Silaghi-Raul-Emanuel/assigments/assigment2/assigment2-2.sh
+++ Labs/homework/S2/Silaghi-Raul-Emanuel/assigments/assigment2/assigment2-2.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <group_name>"
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <group_name> <filename>"
     exit 1
 fi
 
 group_name=$1
+file=$2
 
-group_info=$(getent group "$group_name")
-
-if [ -z "$group_info" ]; then
-    echo "Error: Group '$group_name' not found."
+if [ ! -f "$file" ]; then
+    echo "Error: File '$file' not found."
     exit 1
 fi
 
-users=$(echo "$group_info" | cut -d: -f4)
-
 echo "Group: $group_name"
+users=$(awk -v grp="$group_name" '$2 == grp { print $1 }' "$file")
+
 if [ -z "$users" ]; then
     echo "No users found in this group."
 else
-    echo "Users: $users"
+    echo "Users:"
+    echo "$users"
 fi

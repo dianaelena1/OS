8674e1ccb47dfc9222daf433bcbe47abcf17670d
GabrielRMNC <romanicagabrielmail@gmail.com>
2025-05-26 16:18:09 +0300

    ass78

--- Labs/homework/S1/Pavel_Alex/assignment_7_1.c
+++ Labs/homework/S1/Pavel_Alex/assignment_7_1.c
@@ -1,4 +1,4 @@
-// Write a C Program that receives a command line argument a filename, and prints to standard output the content of this text file, printing also an empty line after each line of text. Make no assumption regarding the maximum length of a line.
+# Write a C Program that receives a command line argument a filename, and prints to standard output the content of this text file, printing also an empty line after each line of text. Make no assumption regarding the maximum length of a line.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,9 +19,9 @@ int main(int argc, char *argv[]) {
     size_t len = 0;
     ssize_t read;
 
-    //while ((read = getline(&line, &len, fp)) != -1) {
-    //    printf("%s\n", line); // Print the line and an extra empty line
-    //}
+    while ((read = getline(&line, &len, fp)) != -1) {
+        printf("%s\n", line); // Print the line and an extra empty line
+    }
 
     free(line);
     fclose(fp);

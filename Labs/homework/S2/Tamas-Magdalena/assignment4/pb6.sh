9811a3e667c3abeac8921fb70cb196c05d7472bf
magdatamas <magda.tamas@yahoo.com>
2025-05-26 10:40:34 +0300

    [Hw 4] Tamas Magdalena pb6

---
+++ Labs/homework/S2/Tamas-Magdalena/assignment4/pb6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#write a Shell program that, starting with the directory given as a first parameter, downwards (recursively), 
+#creates a list of all names that appear (files and directory), and for each filename it prints the number of files with the same name
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <directory>"
+  exit 1
+fi
+
+if [ ! -d "$1" ]; then
+  echo "Error: '$1' is not a directory."
+  exit 2
+fi
+
+find "$1" -print0 | xargs -0 -n1 basename | sort | uniq -c | while read count name; do
+  echo "$name: $count"
+done
